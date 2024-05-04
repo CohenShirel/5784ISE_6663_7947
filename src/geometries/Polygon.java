c43b9d606e9906f1172e6675761b5a5768ee85fe
shirel111 <shirel4201@gmail.com>
2024-05-04 23:38:05 +0300

    try to commit

--- src/geometries/Polygon.java
+++ src/geometries/Polygon.java
@@ -1,5 +1,6 @@
 package geometries;
 
+
 import java.util.List;
 
 import static primitives.Util.isZero;
@@ -50,7 +51,8 @@ public class Polygon extends Geometry {
       // Generate the plane according to the first three vertices and associate the
       // polygon with this plane.
       // The plane holds the invariant normal (orthogonal unit) vector to the polygon
-      plane         = new Plane(vertices[0], vertices[1], vertices[2]);
+     
+      plane=new Plane(vertices[0], vertices[1], vertices[2]);
       if (size == 3) return; // no need for more tests for a Triangle
 
       Vector  n        = plane.getNormal();
