08ace1b224cfff9be3448ebf9852d5be890731e8
noagol100 <noagol100@gmail.com>
2024-06-05 11:38:03 +0300

    COMMIT MY FIXES

--- unitTests/renderer/RenderTests.java
+++ unitTests/renderer/RenderTests.java
@@ -1,63 +1,51 @@
 package renderer;
 
 
-import lighting.AmbientLight;
+import static java.awt.Color.*;
+
 import org.junit.jupiter.api.Test;
-import scene.Scene;
 
 import geometries.*;
+import lighting.AmbientLight;
 import primitives.*;
+import renderer.*;
+import scene.Scene;
 
-/**
- * Test rendering a basic image
- *
- * @author Dan
- */
+/** Test rendering a basic image
+ * @author Dan */
 public class RenderTests {
+   /** Scene of the tests */
+   private final Scene          scene  = new Scene("Test scene");
+   /** Camera builder of the tests */
+   private final Camera.Builder camera = Camera.getBuilder()
+      .setRayTracer(new SimpleRayTrancer(scene))
+      .setLocation(Point.ZERO).setDirection(new Point(0, 0, -1), Vector.Y)
+      .setVpDistance(100)
+      .setVpSize(500, 500);
 
-	/**
-	 * Produce a scene with basic 3D model and render it into a png image with a
-	 * grid
-	 */
-	@Test
-	public void basicRenderTwoColorTest() {
-		Scene scene = new Scene.SceneBuilder("Test scene")
-				.setAmbientLight(new AmbientLight(new Color(255, 191, 191),new Double3(1,1,1)))
-				.setBackground(new Color(75, 127, 90))
-				.setGeometries(new Geometries(new Sphere(new Point(0, 0, -100), 50),
-				new Triangle(new Point(-100, 0, -100), new Point(0, 100, -100), new Point(-100, 100, -100)), // up
-				// left
-				new Triangle(new Point(-100, 0, -100), new Point(0, -100, -100), new Point(-100, -100, -100)), // down
-				// left
-				new Triangle(new Point(100, 0, -100), new Point(0, -100, -100), new Point(100, -100, -100)))).build(); // down
-		// right
-		Camera camera = new Camera(Point.ZERO, new Vector(0, 0, -1), new Vector(0, 1, 0))
-				.setVPDistance(100)
-				.setVPSize(500, 500)
-				.setImageWriter(new ImageWriter("base render test", 1000, 1000))
-				.setRayTracer(new RayTracerBase(scene));
+   /** Produce a scene with basic 3D model and render it into a png image with a
+    * grid */
+   @Test
+   public void renderTwoColorTest() {
+      scene.geometries.add(new Sphere(new Point(0, 0, -100), 50d),
+                           new Triangle(new Point(-100, 0, -100), new Point(0, 100, -100), new Point(-100, 100, -100)), // up
+                           // left
+                           new Triangle(new Point(-100, 0, -100), new Point(0, -100, -100),
+                                        new Point(-100, -100, -100)), // down
+                           // left
+                           new Triangle(new Point(100, 0, -100), new Point(0, -100, -100), new Point(100, -100, -100))); // down
+      scene.ambientLight(new AmbientLight(new Color(255, 191, 191), Double3.ONE))
+         .setBackground(new Color(75, 127, 90));
 
-		camera.renderImage();
-		camera.printGrid(50, new Color(java.awt.Color.YELLOW));
-		camera.writeToImage();
-	}
-	/**
-	 * Produce a scene with basic 3D model and render it into a png image with a
-	 * grid
-	 */	}
-	/**
-	 * Test for XML based scene - for bonus
-	 */
-	//@Test
-//	public void basicRenderXml() {
-//		Scene.SceneBuilder scene = new Scene.SceneBuilder("XML Test scene");
-//
-//		Camera camera = new Camera(Point.ZERO, new Vector(0, 0, -1), new Vector(0, 1, 0)) //
-//				.setVPDistance(100) //
-//				.setVPSize(500, 500)
-//				.setImageWriter(new ImageWriter("xml render test", 1000, 1000))
-//				.setRayTracer(new RayTracerBasic(scene.build()));
-//		camera.renderImage();
-//		camera.printGrid(100, new Color(java.awt.Color.YELLOW));
-//		camera.writeToImage();
-//	}
+      // right
+      camera
+         .setImageWriter(new ImageWriter("base render test", 1000, 1000))
+         .build()
+         .renderImage()
+         .printGrid(100, new Color(YELLOW))
+         .writeToImage();
+   }
+
+}
+
+
